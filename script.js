ea9cce4cf40bc6895c1ecbb32e1579223e6a37b1
DamVanBang <89974383+DamVanBang@users.noreply.github.com>
2025-08-24 14:09:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+<script>
+    // --- JavaScript để điều khiển Menu Mobile ---
+    const menuButton = document.querySelector('.mobile-menu-button');
+    const navMenu = document.querySelector('.main-nav');
+
+    menuButton.addEventListener('click', () => {
+        // Thêm/xóa class 'active' để hiện/ẩn menu
+        navMenu.classList.toggle('active');
+
+        // Thay đổi icon từ hamburger (bars) sang dấu X (times) và ngược lại
+        const icon = menuButton.querySelector('i');
+        if (navMenu.classList.contains('active')) {
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-times');
+        } else {
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        }
+    });
+</script>
