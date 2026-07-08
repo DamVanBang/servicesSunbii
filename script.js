8c9e88a9eb878e195f06b72fcbfcea41670ca6d4
DamVanBang <damvanbang@users.noreply.github.com>
2026-07-08 17:42:56 +0700

    feat: them trang chinh sach bao hanh Pin B1 va nut noi bat

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
+// --- JavaScript để điều khiển Menu Mobile ---
+const menuButton = document.querySelector('.mobile-menu-button');
+const navMenu = document.querySelector('.main-nav');
 
-    // --- JavaScript để điều khiển Menu Mobile ---
-    const menuButton = document.querySelector('.mobile-menu-button');
-    const navMenu = document.querySelector('.main-nav');
+menuButton.addEventListener('click', () => {
+    // Thêm/xóa class 'active' để hiện/ẩn menu
+    navMenu.classList.toggle('active');
 
-    menuButton.addEventListener('click', () => {
-        // Thêm/xóa class 'active' để hiện/ẩn menu
-        navMenu.classList.toggle('active');
-
-        // Thay đổi icon từ hamburger (bars) sang dấu X (times) và ngược lại
-        const icon = menuButton.querySelector('i');
-        if (navMenu.classList.contains('active')) {
-            icon.classList.remove('fa-bars');
-            icon.classList.add('fa-times');
-        } else {
-            icon.classList.remove('fa-times');
-            icon.classList.add('fa-bars');
-        }
-    });
+    // Thay đổi icon từ hamburger (bars) sang dấu X (times) và ngược lại
+    const icon = menuButton.querySelector('i');
+    if (navMenu.classList.contains('active')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    } else {
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
+});
